6d8b3dce4d1bcb9c3bb3f6568af8460612ec4936
Rashid <shadanrashid786@gmail.com>
2025-01-07 03:11:45 +0530

    Error resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,55 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import React, { Suspense } from 'react';
-import Loading from './components/Loading';
-const Home = React.lazy(() => import('./pages/Home'));
-const Resources = React.lazy(() => import('./pages/Resources'));
-const Login = React.lazy(() => import('./pages/Login'));
-const Forum = React.lazy(() => import('./pages/Forum'));
-const Header = React.lazy(() => import('./components/Header'));
-const Register = React.lazy(() => import('./pages/Register'));
-const Helpline = React.lazy(() => import('./pages/Helpline'));
-const AboutUs = React.lazy(() => import('./pages/AboutUs'));
-const NotFound = React.lazy(() => import('./pages/NotFound'));
-const Logout = React.lazy(() => import('./pages/Logout'));
-const IncidentForm = React.lazy(() => import('./pages/IncidentForm'));
-const LoginForm = React.lazy(() => import('./pages/Login'));
-const Map = React.lazy(() => import('./pages/Map'));
-const ContactUs = React.lazy(() => import('./pages/ContactUs'));
-const RegisterForm = React.lazy(() => import('./pages/Register'));
-const AdminLayout = React.lazy(() => import('./components/layouts/admin-layouts'));
-const AdminIncidents = React.lazy(() => import('./pages/AdminIncidents'));
-const AdminContacts = React.lazy(() => import('./pages/AdminContacts'));
-const AdminUsers = React.lazy(() => import('./pages/AdminUsers'));
-const EditUser = React.lazy(() => import('./pages/EditUser')); // Import the EditUser component
-const EditIncident = React.lazy(() => import('./pages/EditIncident')); // Import the EditIncident component
-const EditContact = React.lazy(() => import('./pages/EditContact'));
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import React, { Suspense } from "react";
+import Loading from "./components/Loading";
+const Home = React.lazy(() => import("./pages/Home"));
+const Resources = React.lazy(() => import("./pages/Resources"));
+const Login = React.lazy(() => import("./pages/Login"));
+const Forum = React.lazy(() => import("./pages/Forum"));
+const Header = React.lazy(() => import("./components/Header"));
+const Register = React.lazy(() => import("./pages/Register"));
+const Helpline = React.lazy(() => import("./pages/Helpline"));
+const AboutUs = React.lazy(() => import("./pages/AboutUs"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const Logout = React.lazy(() => import("./pages/Logout"));
+const IncidentForm = React.lazy(() => import("./pages/IncidentForm"));
+const LoginForm = React.lazy(() => import("./pages/Login"));
+const Map = React.lazy(() => import("./pages/Map"));
+const ContactUs = React.lazy(() => import("./pages/ContactUs"));
+const RegisterForm = React.lazy(() => import("./pages/Register"));
+const AdminLayout = React.lazy(() =>
+  import("./components/layouts/admin-layouts")
+);
+const AdminIncidents = React.lazy(() => import("./pages/AdminIncidents"));
+const AdminContacts = React.lazy(() => import("./pages/AdminContacts"));
+const AdminUsers = React.lazy(() => import("./pages/AdminUsers"));
+const EditUser = React.lazy(() => import("./pages/EditUser")); // Import the EditUser component
+const EditIncident = React.lazy(() => import("./pages/EditIncident")); // Import the EditIncident component
+const EditContact = React.lazy(() => import("./pages/EditContact"));
 
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 
-import './index.css';
-import UserProfile from './pages/UserProfile';
+import "./index.css";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   const location = useLocation();
-  
-  const noHeaderFooterRoutes = ['/login', '/register'];
 
-  const shouldHideHeaderFooter = noHeaderFooterRoutes.includes(location.pathname);
+  const noHeaderFooterRoutes = ["/login", "/register"];
 
+  const shouldHideHeaderFooter = noHeaderFooterRoutes.includes(
+    location.pathname
+  );
+  console.log(localStorage.getItem("token"));
   return (
     <div className="app-container">
-      <Suspense fallback={<Loading/>}>
-        {(!shouldHideHeaderFooter) ? <Header /> : null} {/* Conditionally render Header */}
-
+      <Suspense fallback={<Loading />}>
+        {!shouldHideHeaderFooter ? <Header /> : null}{" "}
+        {/* Conditionally render Header */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
@@ -58,16 +67,17 @@ function App() {
             <Route path="incidents" element={<AdminIncidents />} />
             <Route path="users" element={<AdminUsers />} />
             <Route path="contacts" element={<AdminContacts />} />
-            <Route path="users/:id/edit" element={<EditUser />} /> {/* Dynamic route for editing users */}
-            <Route path="incidents/:id/edit" element={<EditIncident />} /> {/* Dynamic route for editing users */}
-            <Route path="contacts/:id/edit" element={<EditContact />} /> {/* Dynamic route for editing users */}
-
+            <Route path="users/:id/edit" element={<EditUser />} />{" "}
+            {/* Dynamic route for editing users */}
+            <Route path="incidents/:id/edit" element={<EditIncident />} />{" "}
+            {/* Dynamic route for editing users */}
+            <Route path="contacts/:id/edit" element={<EditContact />} />{" "}
+            {/* Dynamic route for editing users */}
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
-
-
-        {(!shouldHideHeaderFooter) ? <Footer /> : null} {/* Conditionally render Footer */}
+        {!shouldHideHeaderFooter ? <Footer /> : null}{" "}
+        {/* Conditionally render Footer */}
       </Suspense>
     </div>
   );
