4a7c5a904d5740869dde4978996ea44c8eee0161
Rashid <shadanrashid786@gmail.com>
2025-01-20 02:31:59 +0530

    just

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,6 +26,7 @@ function Login() {
         />
       </div>
       <div className="w-full lg:w-1/2 flex items-center justify-center p-4 lg:p-8">
+        <h1>again cheching</h1>
         <LoginForm className="w-full max-w-md" />
       </div>
     </div>
